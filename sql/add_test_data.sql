ce3c5bdb358c470dbe921f12d3f474d4c10c3b84
Eriksson Andréa C <andreaer@da5-cs-bk107-39.helsinki.fi>
2016-04-04 19:25:48 +0300

    sql komentoja

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,18 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Ingredient (name) VALUES ('light rum');
+INSERT INTO Ingredient (name) VALUES ('cola');
+INSERT INTO Ingredient (name) VALUES ('lime juice');
+
+INSERT INTO Category (name) VALUES ('Cocktail');
+INSERT INTO Category (name) VALUES ('Shot');
+
+INSERT INTO Recipe (name, category, instructions) VALUES ('Cuba Libre', 1,
+	'Squeeze a lime into a Collins glass, add 2 or 3 ice cubes, and pour in
+	 the rum. Fill with cold Coca-Cola. Stir briefly.');
+
+INSERT INTO Recipe_ingredient (ingredient, recipe, amount) VALUES (2, 1, '12 cl');
+INSERT INTO Recipe_ingredient (ingredient, recipe, amount) VALUES (1, 1, '5 cl');
+INSERT INTO Recipe_ingredient (ingredient, recipe, amount) VALUES (3, 1, '1 cl');
+
+INSERT INTO Service_user (name, password) VALUES ('Kalle', 'Kalle123');
+
+
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,28 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Ingredient(
+	id SERIAL PRIMARY KEY,
+	name varchar(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE Category(
+	id SERIAL PRIMARY KEY,
+	name varchar(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE Recipe(
+	id SERIAL PRIMARY KEY,
+	name varchar(50) UNIQUE NOT NULL,
+	category INTEGER REFERENCES Category(id),
+	instructions varchar(600)
+);
+
+CREATE TABLE Recipe_ingredient(
+	ingredient INTEGER REFERENCES Ingredient(id),
+	recipe INTEGER REFERENCES Recipe(id),
+	amount varchar(30) NOT NULL
+);
+
+CREATE TABLE Service_user(
+	id SERIAL PRIMARY KEY,
+	name varchar(50) UNIQUE NOT NULL,
+	password varchar(50) NOT NULL
+);
